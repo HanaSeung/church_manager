104626408568ebfe7d017cf1de4117be77804abd
HanaSeung <lrkns@daum.net>
2026-07-14 09:36:30 +0900

    서버파일 생성

---
+++ 서버시작.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /d "%~dp0"
+title Church Manager - Local Server (port 8002)
+echo.
+echo   Church Manager - Local Server
+echo   ----------------------------------------
+echo   URL    : http://localhost:8002
+echo   Folder : %cd%
+echo   Stop   : close this window or press Ctrl+C
+echo   ----------------------------------------
+echo.
+start "" http://localhost:8002
+python -m http.server 8002
+echo.
+echo   Server stopped. Press any key to close.
+pause >nul
